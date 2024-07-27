d5490920561a4260e871585925516978d8b1a47f
Mohan Bhimavarapu <bhimavarapumohansai@gmail.com>
2024-07-27 13:33:44 +0530

    roboshop changes

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-
-instances=("mongodb","redis","mysql","rabbitmq","catalouge","cart","shipping","payment","web")
+instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
 
 for name in ${instances[@]}; do
-   if [ $name == "shipping" ] || [ $name == "mysql" ]
-   then
-       instance_type= "t3.medium"
-   else
-       instance_type= "t3.micro"
-   fi
-   echo "Creating instance for: $name with instance type: $instance_type"
-done
+    if [ $name == "shipping" ] || [ $name == "mysql" ]
+    then
+        instance_type="t3.medium"
+    else
+        instance_type="t3.micro"
+    fi
+    echo "Creating instance for: $name with instance type: $instance_type"
